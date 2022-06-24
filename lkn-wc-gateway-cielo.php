840e02d21221f8c16800c10c7a97d0d48c25ae6e
emanuellopess <lopes.emanuel@linknacional.com>
2022-06-24 13:39:48 -0300

    improvement: Implement notices if WooCommerce is not available

--- lkn-wc-gateway-cielo.php
+++ lkn-wc-gateway-cielo.php
@@ -11,7 +11,7 @@
  * Text Domain: lkn-wc-gateway-cielo
  * Domain Path: /languages/
  *
- * Requires at least: 4.2
+ * Requires at least: 5.7
  * Tested up to: 6.0
  *
  * Copyright: © 2022 Link Nacional.
@@ -48,6 +48,111 @@ class Lkn_WC_Cielo_Payment {
     }
 
     /**
+     * Check plugin environment
+     *
+     * @return bool|null
+     * @since
+     * @access public
+     *
+     */
+    public static function check_environment() {
+        // Is not admin
+        if (!is_admin() || !current_user_can('activate_plugins')) {
+            return null;
+        }
+
+        // Load plugin helper functions.
+        if (!function_exists('deactivate_plugins') || !function_exists('is_plugin_active')) {
+            require_once ABSPATH . '/wp-admin/includes/plugin.php';
+        }
+
+        // Flag to check whether deactivate plugin or not.
+        $is_deactivate_plugin = false;
+
+        // Verify dependency cases.
+        switch (true) {
+            case doing_action('activate_' . LKN_WC_GATEWAY_CIELO_BASENAME):
+            case doing_action('plugins_loaded'):
+                /* Check to see if Woo is activated, if it isn't deactivate and show a banner. */
+                $is_installed = false;
+
+                if (function_exists('get_plugins')) {
+                    $all_plugins  = get_plugins();
+                    $is_installed = !empty($all_plugins['woocommerce/woocommerce.php']);
+                }
+
+                if (!$is_installed) {
+                    add_action('admin_notices', [__CLASS__, '__lkn_wc_gateway_cielo_dependency_notice']);
+                }
+
+                // Check for if give plugin activate or not.
+                $is_wc_active = class_exists('WooCommerce');
+
+                if (!$is_wc_active) {
+                    add_action('admin_notices', [__CLASS__, '__lkn_wc_gateway_cielo_inactive_notice']);
+
+                    $is_deactivate_plugin = true;
+                }
+
+                break;
+        }
+
+        // Don't let this plugin activate.
+        if ($is_deactivate_plugin) {
+            // Deactivate plugin.
+            deactivate_plugins(LKN_WC_GATEWAY_CIELO_BASENAME);
+
+            if (isset($_GET['activate'])) {
+                unset($_GET['activate']);
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+ * Show plugin dependency notice
+ *
+ * @since
+ */
+    public static function __lkn_wc_gateway_cielo_dependency_notice() {
+        // Admin notice.
+        $message = sprintf(
+            '<strong>%1$s</strong> %2$s <a href="%3$s" target="_blank">%4$s</a>  %5$s %6$s+ %7$s.',
+            __('Activation Error:', 'lkn-wc-gateway-cielo'),
+            __('You must have', 'lkn-wc-gateway-cielo'),
+            'https://wordpress.org/plugins/woocommerce/',
+            __('WooCommerce', 'lkn-wc-gateway-cielo'),
+            __('version', 'lkn-wc-gateway-cielo'),
+            LKN_WC_GATEWAY_CIELO_MIN_WC_VERSION,
+            __('for the Cielo API 3.0 Payments for WooCommerce add-on to activate', 'lkn-wc-gateway-cielo')
+        );
+
+        echo $message;
+    }
+
+    /**
+     * Notice for No Core Activation
+     *
+     * @since
+     */
+    public static function __lkn_wc_gateway_cielo_inactive_notice() {
+        // Admin notice.
+        $message = sprintf(
+            '<div class="notice notice-error"><p><strong>%1$s</strong> %2$s <a href="%3$s" target="_blank">%4$s</a> %5$s.</p></div>',
+            __('Activation Error:', 'lkn-wc-gateway-cielo'),
+            __('You must have', 'lkn-wc-gateway-cielo'),
+            'https://wordpress.org/plugins/woocommerce/',
+            __('WooCommerce', 'lkn-wc-gateway-cielo'),
+            __('plugin installed and activated for the Cielo API 3.0 Payments for WooCommerce add-on to activate', 'lkn-wc-gateway-cielo')
+        );
+
+        echo $message;
+    }
+
+    /**
      * Load the plugin text domain
      *
      * @return void
@@ -73,6 +178,7 @@ class Lkn_WC_Cielo_Payment {
      */
     public static function includes() {
         Lkn_WC_Cielo_Payment::setup_constants();
+        Lkn_WC_Cielo_Payment::check_environment();
 
         // Make the Lkn_WC_Gateway_Cielo_Credit class available.
         if (class_exists('WC_Payment_Gateway')) {
@@ -90,7 +196,18 @@ class Lkn_WC_Cielo_Payment {
         // Defines addon version number for easy reference.
         if (!defined('LKN_WC_CIELO_VERSION')) {
             define('LKN_WC_CIELO_VERSION', '1.0.0');
+        }
+        if (!defined('LKN_WC_CIELO_TRANSLATION_PATH')) {
             define('LKN_WC_CIELO_TRANSLATION_PATH', plugin_dir_path(__FILE__) . 'languages/');
+        }
+        if (!defined('LKN_WC_GATEWAY_CIELO_BASENAME')) {
+            define('LKN_WC_GATEWAY_CIELO_BASENAME', plugin_basename(__FILE__));
+        }
+        if (!defined('LKN_WC_GATEWAY_CIELO_DIR')) {
+            define('LKN_WC_GATEWAY_CIELO_DIR', plugin_dir_path(__FILE__));
+        }
+        if (!defined('LKN_WC_GATEWAY_CIELO_MIN_WC_VERSION')) {
+            define('LKN_WC_GATEWAY_CIELO_MIN_WC_VERSION', '5.0.0');
         }
     }
 
