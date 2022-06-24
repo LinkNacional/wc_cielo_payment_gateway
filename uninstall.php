d706447d7c7de2946853a9a61a6c97d8b56c94d4
emanuellopess <lopes.emanuel@linknacional.com>
2022-06-24 13:37:56 -0300

    feat: Implement uninstall script

---
+++ uninstall.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Uninstall Lkn_WC_Cielo_Payment
+ *
+ * @license     https://opensource.org/licenses/gpl-license GNU Public License
+ * @since       1.0
+ */
+
+// Exit if accessed directly.
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    exit;
+}
+
+// Clear WooCommerce Gateway options
+delete_option('woocommerce_lkn_cielo_credit_settings');
+delete_option('woocommerce_lkn_cielo_debit_settings');
