ae2209620a4f3836a1499a3bff766f0bda61a096
Emanuel Lopes <lopes.emanuel@linknacional.com>
2023-11-24 20:44:50 +0000

    feat: Better notice for plugin PRO selling #31

--- resources/js/admin/lkn-wc-gateway-admin.js
+++ resources/js/admin/lkn-wc-gateway-admin.js
@@ -5,7 +5,17 @@ if (adminPage && (adminPage === 'lkn_cielo_credit' || adminPage === 'lkn_cielo_d
   const noticeDiv = document.createElement('div')
   noticeDiv.setAttribute('style', 'padding: 10px 5px;background-color: #fcf9e8;color: #646970;border: solid 1px lightgrey;border-left-color: #dba617;border-left-width: 4px;font-size: 14px;min-width: 625px;margin-top: 10px;')
 
-  noticeDiv.innerHTML = 'Obtenha novas funcionalidades com <a href="https://www.linknacional.com.br/wordpress/woocommerce/cielo/" target="_blank">Cielo API Pro</a>'
+  noticeDiv.innerHTML = '<div style="font-size: 21px;padding: 6px 0px 10px 0px;">Obtenha novas funcionalidades com Cielo API Pro</div>' +
+  '<ul style="margin: 10px 28px;list-style: disclosure-closed;">' +
+    '<li>Captura manual da transação/pedido</li>' +
+    '<li>Ferramenta de reembolso total ou parcial</li>' +
+    '<li>Compatibilidade com pagamentos feitos em moedas internacionais</li>' +
+    '<li>Ajustes da taxa de juros de acordo com a parcela</li>' +
+    '<li>Habilita o parcelamento em até 18x (Visa, Elo, Amex, Hipercard, Mastercard)</li>' +
+    '<li>Configuração de quantidade máxima de parcelas</li>' +
+    '<li>Compatibilidade com a opção de Checkout do Elementor para WooCommerce</li>' +
+  '</ul>' +
+  '<a href="https://www.linknacional.com.br/wordpress/woocommerce/cielo/" target="_blank">Conheça e compre o plugin PRO</a>'
 
   wcForm.append(noticeDiv)
 }
